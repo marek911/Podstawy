cb2c503e4ea06847f896109e0a0c070dc88dc123
Marek Solarz <solarz.mar@gmail.com>
2019-12-01 10:58:32 +0100

    sortowanie przez wybieranie

--- src/main/java/podstawy/zadania/Sortowanie/Wybieranie.java
+++ src/main/java/podstawy/zadania/Sortowanie/Wybieranie.java
@@ -2,28 +2,29 @@ package podstawy.zadania.Sortowanie;
 
 import java.util.Arrays;
 
-public class Wstawianie {
+public class Wybieranie {
+
     public static void main(String[] args) {
-        int tablica [] = {8,1,14,20,7,45};
+        int tablica [] = {8,2,14,20,7,45};
         System.out.println("nieposortowana tablica: " + Arrays.toString(tablica));
         selectionsort(tablica);
         System.out.println("posortowana tablica :" + Arrays.toString(tablica));
 
     }
-    
+
         public static void selectionsort(int[]tablica){
 
         for (int i=0; i<tablica.length; i++){
-            int min = tablica[i];
             int indeks =i;
-            for (int j=i; j<tablica.length;j++){
-                if(tablica[j]<min){
-                    min= tablica[j];
-                    indeks =j;
+            for (int j=i; j<tablica.length;j++) {
+                if (tablica[j] < tablica[i]) {
+                    indeks = j;
+
+                    int temp = tablica[i];
+                    tablica[i] = tablica[indeks];
+                    tablica[indeks] = temp;
                 }
             }
-            tablica[indeks]=tablica[i];
-            tablica[i]=min;
 
         }
     }
