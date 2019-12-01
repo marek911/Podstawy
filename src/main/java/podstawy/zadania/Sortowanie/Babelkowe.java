ebb0f1200cf78c9e770a93be199156dc983ab6b4
Marek Solarz <solarz.mar@gmail.com>
2019-12-01 09:58:21 +0100

    sortowanie

--- src/main/java/podstawy/zadania/Sortowanie/Babelkowe.java
+++ src/main/java/podstawy/zadania/Sortowanie/Babelkowe.java
@@ -1,28 +1,33 @@
 package podstawy.zadania.Sortowanie;
 
+import com.sun.org.apache.xpath.internal.objects.XBoolean;
+
 import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class Babelkowe {
     public static void main(String[] args) {
 
-int[]tablica = {11,50,7,4,24,1,37};
+        int[] tablica = {11, 50, 7, 4, 24, 1, 37};
         System.out.println("nieposortowna tablica :" + Arrays.toString(tablica));
 
 
-        for (int i = 0; i < tablica.length-1; i++) {
-            for (int j = 0; j < tablica.length-1; j++) {
-                if (tablica[j] > tablica[j + 1]){
-                swap(tablica, j,j + 1);
-                 }
+        for (int i = 0; i < tablica.length - 1; i++) {
+            for (int j = 0; j < tablica.length - 1; j++) {
+                if (tablica[j] > tablica[j + 1]) {
+                    swap(tablica, j, j + 1);
+                }
             }
+
         }
-        System.out.println("posortowna tablica :" + Arrays.toString(tablica));
+        System.out.println("posortowana tablica :" + Arrays.toString(tablica));
+
     }
         private static void swap(int[] tablica, int i, int j) {
         int temp = tablica[i];
         tablica[i] = tablica[j];
         tablica[j] = temp;
     }
+
     }
 
